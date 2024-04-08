e0a994281a4512e0c4bae92311fc8a60889a290e
hxf13 <2994198039@qq.com>
2024-04-08 16:53:03 +0800

    1

---
+++ spring-boot-exception/src/main/java/top/xiufenghuang/config/MybatisPlusPageConfig.java
@@ -0,0 +1,25 @@
+package top.xiufenghuang.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 功能：
+ * 作者：程序员青戈
+ * 日期：2024/4/1 19:54
+ */
+@Configuration
+public class MybatisPlusPageConfig {
+    /**
+     * 新的分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
